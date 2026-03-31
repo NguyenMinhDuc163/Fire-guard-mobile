8badef9ec921f61a73fcf3f49dd1b9868dcaf217
Nguyen Duc <DucNM.B21CN249@stu.ptit.edu.vn>
2026-03-31 23:19:01 +0700

    add new CMake configuration files and update dependencies in pubspec.yaml

--- lib/service/common/status_api.dart
+++ lib/service/common/status_api.dart
@@ -1,11 +1,6 @@
 class StatusApi {
-  static String BASE_API_URL = "https://iot.nguyenduc.click/api/v1/"; // server
-  static String URL = "https://iot.nguyenduc.click"; // server
-// static String URL = "http://192.168.190.125:3000"; // server
-// static String BASE_API_URL = "http://192.168.0.104:3000/api/v1/"; // test nha
-// static String BASE_API_URL = "http://192.168.100.171:3000/api/v1/"; // test cty
-// static String BASE_API_URL = "http://192.168.190.125:3000/api/v1/"; // test cty
-// static String BASE_API_URL = "http://20.2.33.95:3000/api/v1/"; // server
+  static String BASE_API_URL = "https://iot.ndtech.io.vn/api/v1/"; // server
+  static String URL = "https://iot.ndtech.io.vn/api/v1/"; // server
 
   static const int TIME_OUT = 10000;
 }
